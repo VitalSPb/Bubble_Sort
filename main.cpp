0e39fd51319a58fdd5098739461d2f5c184038fd
VitalSPb <62208768+VitalSPb@users.noreply.github.com>
2020-12-08 06:44:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 
 
 int main() {
+// Объявление и инициализация неотсортированного массива.
 	const int size = 5;
 	int arr[size] = { 4,5,1,2,3 };
-
+// Вывод неотсортированного массива.
 	for (int i = 0; i < size; i++) {
 		std::cout << arr[i] << " ";
 	}
+	
+	std::cout << std::endl;
+// Сортировка массива	
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size - 1 - i; j++) {
+			if (arr[j] > arr[j + 1])
+			{
+				std::swap(arr[j], arr[j + 1];
+			}
+		}
+	}
+// Вывод отсортированного массива.
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
+	}					  
 }
