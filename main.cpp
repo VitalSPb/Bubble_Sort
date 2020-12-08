02ba65c9d06c8598205513f623d4c88a2fb1df35
VitalSPb <vonov1976@gmail.com>
2020-12-08 06:57:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,13 @@ int main() {
 	}
 	
 	std::cout << std::endl;
+
 // Сортировка массива	
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size - 1 - i; j++) {
 			if (arr[j] > arr[j + 1])
 			{
-				std::swap(arr[j], arr[j + 1];
+				std::swap(arr[j], arr[j + 1]);
 			}
 		}
 	}
